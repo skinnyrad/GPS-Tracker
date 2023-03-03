1f7138d28d6d518690dc99202618cda038362785
Relpek1 <124194193+Relpek1@users.noreply.github.com>
2023-03-02 21:56:10 -0600

    Update GPS-Tracker-Code.ino
    Added comments and removed pointless code.

--- GPS-Tracker-Code/GPS-Tracker-Code.ino
+++ GPS-Tracker-Code/GPS-Tracker-Code.ino
@@ -19,7 +19,7 @@ TinyGPSPlus gps;
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
 
-void appendFile(fs::FS &fs, const char * path, const char * message){ // Function to write information to SD card
+void appendFile(fs::FS &fs, const char * path, const char * message){ // function to write information to SD card
     Serial.printf(" Appending to file: %s ", path);
     File file = fs.open(path, FILE_APPEND);
     if(!file){
@@ -40,16 +40,16 @@ void setup(){
     ss.begin(GPSBaud); // GPS  
 
     if(!SD.begin()){
-        Serial.println("Card Mount Failed"); // This either means you need to check the wiring or if the actual SD card isn't there
+        Serial.println("Card Mount Failed"); // this either means you need to check the wiring or if the actual SD card isn't there
         return;
     }
-    uint8_t cardType = SD.cardType();
-
-    if(cardType == CARD_NONE){
+   
+    // debugging info
+    uint8_t cardType = SD.cardType(); // initializing cardType for debugging
+    if(cardType == CARD_NONE){ // checking if a SD card is in the reader
         Serial.println("No SD card attached");
         return;
     }
-
     Serial.print("SD Card Type: ");
     if(cardType == CARD_MMC){
         Serial.println("MMC");
@@ -60,24 +60,29 @@ void setup(){
     } else {
         Serial.println("UNKNOWN");
     }
-
     uint64_t cardSize = SD.cardSize() / (1024 * 1024);
     Serial.printf("SD Card Size: %lluMB\n", cardSize);
     Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
     Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
-
-    while(!gps.date.isValid() || !gps.time.isValid()){delay(1000);}
-    previous_location_lat = gps.location.lat(); // Saving previous location for weeding out extraneous Latitude data
-    previous_location_lng = gps.location.lng(); // Saving previous location for weeding out extraneous Longitude data
+   
+    // waiting for GPS to aquire date and time 
+    while(!gps.date.isValid() || !gps.time.isValid()){delay(1000);} // the program will not continue until both date and time are available
+    
+    // saving current location for weeding out extraneous location data
+    previous_location_lat = gps.location.lat(); 
+    previous_location_lng = gps.location.lng(); 
+ 
+    // naming the file
     File0 = "/GpsData_"+String(gps.date.month())+"_"+String(gps.date.day())+"_"+String(gps.date.year())+"_"+String(gps.time.hour())+".csv";
     F = File0.c_str();
+ 
     // save data header
     appendFile(SD, F,"Date [mm/dd/yyyy], Time [HH:MM:SS.ZZ], Latitude [deg], Longitude [deg], Speed [kmph]\n");
     
 }
 
 void loop(){
-  // Exicute code only if there is a SD card detected
+  // exicute code only if there is a SD card detected
   if(SD.cardType() != CARD_NONE){ 
 
   while (ss.available() > 0){
@@ -91,26 +96,26 @@ void loop(){
       data_to_save += String(gps.location.lng(),8)+","; // longitude
       data_to_save += String(gps.speed.kmph()); // Speed
       data_to_save += "\n";
-
+      
+      // if the GPS location is suspiciously jumpy (10 degrees of logitude or latitude movement between loops) the data is ignored
       if(abs(previous_location_lat - gps.location.lat()) < 10 && abs(previous_location_lng - gps.location.lng()) < 10){
       
         const char* d = data_to_save.c_str(); // convert data string into const char * (so it is compatible with appendFile function)
         appendFile(SD, F,d); // save new data points
         Serial.println(d); // uncomment to print GPS data
       }
-      previous_location_lat = gps.location.lat();
-      previous_location_lng = gps.location.lng();
+     
+      // saving current location for weeding out extraneous location data
+      previous_location_lat = gps.location.lat(); 
+      previous_location_lng = gps.location.lng(); 
+     
       delay(100);  // wait one tenth of a sec before looping  
     }
   }
-  }else{delay(1000);// If the SD card is not detected, wait a sec
+  }else{delay(1000);// if the SD card is not detected, wait a sec
     Serial.println("No SD card attached");
         if(!SD.begin()){
         Serial.println("Card Mount Failed");
-    }} 
-  if (millis() > 5000 && gps.charsProcessed() < 10) // checking to see if the GPS is sending data
-  {
-    Serial.println(F("No GPS detected: check wiring."));
-    delay(1000);
-  }
+    }
+  } 
 }
