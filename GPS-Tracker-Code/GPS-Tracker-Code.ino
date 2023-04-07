b039aa0f109220eba97f1e9e4fd21c6b41a42f6b
Relpek1 <124194193+Relpek1@users.noreply.github.com>
2023-04-06 21:27:13 -0500

    Update GPS-Tracker-Code.ino
    Added the fix to file naming
    Cut out a lot of unnecessary, broken, and outdated code

--- GPS-Tracker-Code/GPS-Tracker-Code.ino
+++ GPS-Tracker-Code/GPS-Tracker-Code.ino
@@ -1,23 +1,18 @@
- /*
+/*
 * GPS-Tracker
 */
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
 #include "TinyGPSPlus.h"
-#include "SoftwareSerial.h"
 
-static const int RXPin = 3, TXPin = 1;
-static const uint32_t GPSBaud = 9600;
 double previous_location_lat;
 double previous_location_lng;
-String File0 = "Failed"; // The initial state of the file name is "Failed" in order to debug later.
+String File0;
 const char* F;
+
 // The TinyGPSPlus object
 TinyGPSPlus gps;
-
-// The serial connection to the GPS device
-SoftwareSerial ss(RXPin, TXPin);
 
 void appendFile(fs::FS &fs, const char * path, const char * message){ // function to write information to SD card
     Serial.printf(" Appending to file: %s ", path);
@@ -35,29 +30,30 @@ void appendFile(fs::FS &fs, const char * path, const char * message){ // functio
 }
 
 void setup(){
+  
+    Serial.begin(9600); // Serial Moniter
     
-    Serial.begin(19200); // Serial Moniter
-    ss.begin(GPSBaud); // GPS  
-
-      Serial.println("Mounting Card...");
-
     if(!SD.begin()){
-        Serial.println("Card Mount Failed"); // this either means you need to check the wiring or if the actual SD card isn't there
-        return;
+      Serial.println("Card Mount Failed"); // this either means you need to check the wiring or if the actual SD card isn't there
+      return;
+    }else{
+    Serial.println("Card Mount Successful");
     }
-   
+
     // debugging info
-    uint8_t cardType = SD.cardType(); // initializing cardType for debugging
-    if(cardType == CARD_NONE){ // checking if a SD card is in the reader
-        Serial.println("No SD card attached");
+
+    if(SD.cardType() == CARD_NONE){ // checking if a SD card is in the reader
+     
+        Serial.println("No SD card attached, check connections");
         return;
     }
+
     Serial.print("SD Card Type: ");
-    if(cardType == CARD_MMC){
+    if(SD.cardType() == CARD_MMC){
         Serial.println("MMC");
-    } else if(cardType == CARD_SD){
+    } else if(SD.cardType() == CARD_SD){
         Serial.println("SDSC");
-    } else if(cardType == CARD_SDHC){
+    } else if(SD.cardType() == CARD_SDHC){
         Serial.println("SDHC");
     } else {
         Serial.println("UNKNOWN");
@@ -67,14 +63,18 @@ void setup(){
     Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
     Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
    
+    // waiting for GPS to aquire date and time 
+    while(!gps.date.isValid() || !gps.time.isValid()) // the program will not continue until both date and time are available
+    {
+      if(Serial.available()>0)
+      {
+        gps.encode(Serial.read());
+      }else{
+      Serial.println("Searching for satilites...");
+      delay(100);
+      }
+    }
     
-
-    /*
-    * // waiting for GPS to aquire date and time (commented out for testing)
-    * while (File0 == "Failed"){
-    * if (gps.encode(ss.read()) && gps.location.isValid() && gps.date.isValid() && gps.time.isValid()){
-    */
-
     // saving current location for weeding out extraneous location data
     previous_location_lat = gps.location.lat(); 
     previous_location_lng = gps.location.lng(); 
@@ -82,36 +82,30 @@ void setup(){
     // naming the file
     File0 = "/GpsData_"+String(gps.date.month())+"_"+String(gps.date.day())+"_"+String(gps.date.year())+"_"+String(gps.time.hour())+".csv";
     F = File0.c_str();
-    
-    // Saving the file name dates for debugging
-    String j = String(gps.date.month())+"_"+String(gps.date.day())+"_"+String(gps.date.year())+"_"+String(gps.time.hour()); 
-    const char* y = j.c_str();
-    appendFile(SD, F,y);
 
-    // save data header
+    // save data header (a single data file could have multiple save data headers, this is intended to separate the resets and should prevent the GPS visualizer from connecting points that are too far from each other)
     appendFile(SD, F,"Date [mm/dd/yyyy], Time [HH:MM:SS.ZZ], Latitude [deg], Longitude [deg], Speed [kmph]\n");
-  
-    //}
-  //}
 }
 
 void loop(){
-  
-  while (ss.available() > 0){
-    if (gps.encode(ss.read()) && gps.location.isValid() && gps.date.isValid() && gps.time.isValid()){
+  // even if the SD card is removed for a period of time, the program will just continue off where it started once the SD card is replaced
+  while (Serial.available() > 0)
+  {
+    if (gps.encode(Serial.read()) && gps.location.isValid() && gps.date.isValid() && gps.time.isValid())
+    {
       String data_to_save = ""; // data string for saving
-      data_to_save += String(gps.date.month())+"/"+String(gps.date.day())+"/"+  // date
-                      String(gps.date.year())+",";
-      data_to_save += String(gps.time.hour())+":"+String(gps.time.minute())+":"+  //  time
-                      String(gps.time.second())+"."+String(gps.time.centisecond())+",";
+      data_to_save += String(gps.date.month())+"/"+String(gps.date.day())+"/"+  // month & day
+                      String(gps.date.year())+","; // year
+      data_to_save += String(gps.time.hour())+":"+String(gps.time.minute())+":"+  //  hour & minute
+                      String(gps.time.second())+"."+String(gps.time.centisecond())+","; // second & centisecond
       data_to_save += String(gps.location.lat(),8)+","; // latitude
       data_to_save += String(gps.location.lng(),8)+","; // longitude
       data_to_save += String(gps.speed.kmph()); // Speed
       data_to_save += "\n";
-      
+
       // if the GPS location is suspiciously jumpy (10 degrees of logitude or latitude movement between loops) the data is ignored
-      if(abs(previous_location_lat - gps.location.lat()) < 10 && abs(previous_location_lng - gps.location.lng()) < 10){
-      
+      if(abs(previous_location_lat - gps.location.lat()) < 10 && abs(previous_location_lng - gps.location.lng()) < 10)
+      {
         const char* d = data_to_save.c_str(); // convert data string into const char * (so it is compatible with appendFile function)
         appendFile(SD, F,d); // save new data points
         Serial.println(d); // uncomment to print GPS data
@@ -120,7 +114,8 @@ void loop(){
       // saving current location for weeding out extraneous location data
       previous_location_lat = gps.location.lat(); 
       previous_location_lng = gps.location.lng(); 
-     
+    
       delay(100);  // wait one tenth of a sec before looping  
     }
   }
+}
